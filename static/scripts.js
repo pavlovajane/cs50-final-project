03703164c4a138337bd929ebd4b3312fca4c890b
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-05-09 20:59:09 -0400

    Fix input event listener for registration

--- static/scripts.js
+++ static/scripts.js
@@ -7,19 +7,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
   // At least one digit
   // At least one special character
   const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
-  passwordRegisterInput.addEventListener('input', function () {
-
+  passwordRegisterInput.addEventListener('input', function (event) {    
     const isValid = passwordRegex.test(this.value);
 
     // If the input value is valid, set the input field's background color to green
     if (isValid) {
-      passwordRegisterInput.style.backgroundColor = 'lightgreen !important';
-      console.log('Input is valid');
+      event.target.style.backgroundColor = '#90ee90';
     }
     // Otherwise, set the input field's background color to red
     else {
-      passwordRegisterInput.style.backgroundColor = 'lightred !important';
-      console.log('Input is not valid');
+      event.target.style.backgroundColor = '#f08080';
     }
   });
 });

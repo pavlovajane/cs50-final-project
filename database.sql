16a3f45fa5f7ba2d98a3bbc35e28c748bd4ad8d6
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-05-12 11:55:38 -0400

    Add database tables scripts

--- database.sql
+++ database.sql
@@ -3,17 +3,27 @@ CREATE TABLE users (
     username TEXT NOT NULL, hash TEXT NOT NULL);
 
 CREATE TABLE runs (
-   id INTEGER NOT NULL,
-   user_id INTEGER,
-    symbol TEXT NOT NULL,
-    operation TEXT NOT NULL,
-    share INTEGER NOT NULL,
-    price REAL NOT NULL,
-    amount REAL NOT NULL,
+    id INTEGER NOT NULL,
     rundate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    distance REAL NOT NULL,
     runtime TEXT NOT NULL,
+    speed REAL NOT NULL,
+    weather TEXT,
+    notes TEXT,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users (id) 
 );
+
+CREATE TABLE marathoners (
+    id INTEGER NOT NULL,
+    marathon TEXT NOT NULL,
+    athlete TEXT NOT NULL DEFAULT "anonymous runner",
+    agecategory TEXT,
+    km4week REAL NOT NULL,
+    speed4week REAL NOT NULL
+    crosstraining TEXT,
+    marathontime REAL NOT NULL,
+    performancecategory CHARACTER(1)
+)
 
 CREATE INDEX user_id ON runs(user_id);
